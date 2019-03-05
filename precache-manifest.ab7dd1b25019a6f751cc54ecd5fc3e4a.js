aecee13b51dfea0c80608573020c898aff0de950
Marko <marko.perendic@yahoo.com>
2019-03-05 13:21:16 +0100

    Updates

---
+++ precache-manifest.ab7dd1b25019a6f751cc54ecd5fc3e4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c417dc263937fad15c82",
+    "url": "/pomodoro-clock/static/js/runtime~main.c417dc26.js"
+  },
+  {
+    "revision": "f22096c303aed3c95c27",
+    "url": "/pomodoro-clock/static/js/main.f22096c3.chunk.js"
+  },
+  {
+    "revision": "44bccae12b9632b8611f",
+    "url": "/pomodoro-clock/static/js/2.44bccae1.chunk.js"
+  },
+  {
+    "revision": "f22096c303aed3c95c27",
+    "url": "/pomodoro-clock/static/css/main.fec79c0d.chunk.css"
+  },
+  {
+    "revision": "47d0eb556cd38e6ac0263586a6572e00",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
